25633ca4f7bbea9271ee964aef180c1a2b71e958
web-lux <web-lux@outlook.fr>
2023-10-12 16:43:11 +0200

    feat: css of Error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,13 +1,20 @@
 import Header from "./Components/Header.jsx"
 import Footer from "./Components/Footer.jsx"
+import { Link } from "react-router-dom";
+import style from "./ErrorPage.module.scss"
 
 export default function ErrorPage() {
 
     return (
-      <>
-        <Header />
-        <p>ErrorPage</p>
-        <Footer />
-      </>
+      <div className={style.error}>
+          <Header />
+          <main>
+
+            <h1>404</h1>
+            <p>La page demandée n'existe pas. <Link to="/">Retourner à l'index ?</Link></p>
+
+          </main>
+          <Footer />
+      </div>
     )
   }
