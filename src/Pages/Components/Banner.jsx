c1ed89f634ae36d049aeec9666d7278b26c92377
web-lux <web-lux@outlook.fr>
2023-10-30 00:23:43 +0100

    fix: broken img on Vercel

--- src/Pages/Components/Banner.jsx
+++ src/Pages/Components/Banner.jsx
@@ -1,25 +1,29 @@
 import style from "./Banner.module.scss";
+import truckImg from "../../assets/truck.svg";
+import headphoneImg from "../../assets/headphone.svg";
+import shieldImg from "../../assets/shield.svg";
+import smileImg from "../../assets/smile.svg";
 
 export default function Banner() {
 	return (
 		<ul aria-label="Avantages de Run'zik" className={style.banner}>
 			<li>
-				<img src="/src/assets/truck.svg" alt="" />
+				<img src={truckImg} alt="" />
 				<span>Livraison gratuite en Europe</span>
 			</li>
 
 			<li>
-				<img src="/src/assets/headphones.svg" alt="" />
+				<img src={headphoneImg} alt="" />
 				<span>Service après-vente réactif</span>
 			</li>
 
 			<li>
-				<img src="/src/assets/shield.svg" alt="" />
+				<img src={shieldImg} alt="" />
 				<span>Garantie 2 ans</span>
 			</li>
 
 			<li>
-				<img src="/src/assets/smile.svg" alt="" />
+				<img src={smileImg} alt="" />
 				<span>Satisfait ou remboursé sous 15 jours</span>
 			</li>
 		</ul>
